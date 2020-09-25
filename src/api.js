7a15529e5b375e9ffc6ba94539d9ad6aba25b894
Grant Miller <gmmiller@mit.edu>
2020-09-24 19:12:03 -0700

    Trying to fix some bugs

--- src/api.js
+++ src/api.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const _ = require("lodash");
 const { titleCase } = require('title-case');
+const fs = require('fs');
+
 
 const Emoji = require('./schemas/emoji');
 
@@ -12,45 +14,47 @@ const ELO_SCALE = 32;
 
 function randomEmoji() {
   const emojiKeys = Object.keys(emojiByName);
-  const randomIndex = Math.floor(Math.random()*emojiKeys.length);
+  const randomIndex = Math.floor(Math.random() * emojiKeys.length);
   const key = emojiKeys[randomIndex];
   return { key: key, emoji: emojiByName[key] };
 }
 
-router.get('/emoji', (req,res)=> {
+router.get('/emoji', (req, res) => {
   emoji1 = randomEmoji();
   emoji2 = randomEmoji();
-  while(emoji1.key === emoji2.key) {
+  while (emoji1.key === emoji2.key) {
     emoji2 = randomEmoji();
   }
-  
+
   res.send({
     0: {
       'emoji': emoji1.emoji,
-      'key':emoji1.key
+      'key': emoji1.key
     },
     1: {
       'emoji': emoji2.emoji,
-      'key':emoji2.key
+      'key': emoji2.key
     }
   });
 });
 
-router.post('/vote', (req,res) => {
-  const {winner, loser} = req.body;
+router.post('/vote', (req, res) => {
+  const { winner, loser } = req.body;
 
-  Emoji.find({'name': {
-    $in: [winner,loser]
-  }}).then(results => {
-    const winnerEmoji = _.find(results, {'name':winner});
-    const loserEmoji = _.find(results, {'name':loser});
+  Emoji.find({
+    'name': {
+      $in: [winner, loser]
+    }
+  }).then(results => {
+    const winnerEmoji = _.find(results, { 'name': winner });
+    const loserEmoji = _.find(results, { 'name': loser });
 
     const winnerELO = winnerEmoji !== undefined ? winnerEmoji.elo : 1000;
-    const loserELO  = loserEmoji !== undefined ? loserEmoji.elo : 1000;
+    const loserELO = loserEmoji !== undefined ? loserEmoji.elo : 1000;
 
-    const expectedOutcome = (1.0/(1.0 + Math.pow(10, ((winnerELO-loserELO)/400))));
-    const winnerUpdated = winnerELO + ELO_SCALE * (1-expectedOutcome);
-    const loserUpdated = loserELO + ELO_SCALE *(-expectedOutcome);
+    const expectedOutcome = (1.0 / (1.0 + Math.pow(10, ((winnerELO - loserELO) / 400))));
+    const winnerUpdated = winnerELO + ELO_SCALE * (1 - expectedOutcome);
+    const loserUpdated = loserELO + ELO_SCALE * (-expectedOutcome);
 
     if (winnerEmoji) {
       winnerEmoji.elo = winnerUpdated;
@@ -66,7 +70,7 @@ router.post('/vote', (req,res) => {
       newEmoji.save();
     }
 
-    if(loserEmoji) {
+    if (loserEmoji) {
       loserEmoji.elo = loserUpdated;
       loserEmoji.losses += 1;
       loserEmoji.save();
@@ -79,46 +83,64 @@ router.post('/vote', (req,res) => {
       });
       newEmoji.save();
     }
-    
+
   });
 
   res.send({});
 });
 
 
-router.get('/rankings', (req,res) => {
-  Emoji.find({}).sort({'elo': 'descending'}).then(result => {
+router.get('/rankings', (req, res) => {
+  Emoji.find({}).sort({ 'elo': 'descending' }).then(result => {
     const top = [];
     const bottom = [];
     const numEmojis = Math.min(EMOJIS_PER_TABLE, result.length);
 
-    for(var i = 0; i < numEmojis; i++) {
+    for (var i = 0; i < numEmojis; i++) {
       let entry = result[i];
       top.push({
-        'rank': i+1,
+        'rank': i + 1,
         'emoji': emojiByName[entry.name],
         'elo': Math.round(entry.elo),
         'name': titleCase(entry.name.toString().replace(/[_-]/g, ' '))
       });
     }
 
-    for(var i = result.length - 1 ; i >= result.length - numEmojis; i--) {
+    for (var i = result.length - 1; i >= result.length - numEmojis; i--) {
       let entry = result[i];
       bottom.push({
-        'rank': i+1,
+        'rank': i + 1,
         'emoji': emojiByName[entry.name],
         'elo': Math.round(entry.elo),
         'name': titleCase(entry.name.toString().replace(/[_-]/g, ' '))
       });
     }
 
-    res.send({top:top, bottom:bottom});
+    res.send({ top: top, bottom: bottom });
   });
-}); 
+});
 
-// router.get('/delete', (req,res) => {
-//   Emoji.deleteMany({}, () => {});
-//   res.send({});
+router.get('/total-votes', (req, res) => {
+  Emoji.find({}).then(results => {
+    let sum = 0;
+    results.forEach(entry => {
+      sum += entry.wins + entry.losses;
+    });
+    res.send({ totalVotes: sum });
+  });
+});
+
+
+// router.get('/save-info', (req, res) => {
+//   Emoji.find({}).then(results => {
+//     var file = fs.createWriteStream(`${Date.now()}-backup.txt`);
+//     file.on('error', function (err) { res.send({'error':'Failed to save'}) });
+//     results.forEach(function (v) { file.write(v.toString() + ', \n')});
+//     file.end();
+//     res.send({});
+//   });
 // });
+
+
 
 module.exports = router;
