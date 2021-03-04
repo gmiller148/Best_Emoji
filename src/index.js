af43729e760844645c36dcf424628c77cd4301cd
Grant Miller <gmmiller@mit.edu>
2021-03-04 11:22:06 -0600

    Idk what Im doing

--- src/index.js
+++ src/index.js
@@ -10,7 +10,6 @@ const _ = require('lodash');
 const helmet = require('helmet');
 
 const app = express();
-app.use(helmet());
 const server = http.createServer(app);
 
 // set POST request body parser
